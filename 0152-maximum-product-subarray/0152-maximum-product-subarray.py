4896848012b176a6615822a06cfa1887abc9fb46
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2026-07-04 04:15:21 +0530

    Time: 7 ms (40.41%), Space: 19.9 MB (43.7%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -7,7 +7,7 @@ class Solution:
             Variable_1 = max_ending * nums[i]
             Variable_2 = nums[i]
             Variable_3 = min_ending * nums[i]
-            max_ending = max(Variable_1,max(Variable_2,Variable_3))
-            min_ending = min(Variable_1,min(Variable_2,Variable_3))
-            res = max(res,max(max_ending,min_ending))
+            max_ending = max(Variable_1,Variable_2,Variable_3)
+            min_ending = min(Variable_1,Variable_2,Variable_3)
+            res = max(res,max_ending,min_ending)
         return res
