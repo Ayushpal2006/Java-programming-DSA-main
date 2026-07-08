d67ccfa2a8c960b93c8ac58c8fbcf4e78a9529e9
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2026-07-09 04:36:54 +0530

    Correct prefix sum logic in prefix_practice.py
    Fix prefix sum calculation in prefix_practice.py.

--- practice/prefix_practice.py
+++ practice/prefix_practice.py
@@ -4,6 +4,10 @@ def prefSum(arr):
     preq = [0]*n
     preq[0]=arr[0]
 
+    for i in range(1,n):
+        preq[i]=preq[i-1]+arr[i-1]
+    return preq
+
 if __name__ == "__main__":
     arr = [10, 20, 10, 5, 15]
     prefixSum = prefSum(arr)
